0214168a1848193c9f0a44117ea5cd633df73da3
Vaidik Gupta <vaidikgupta2000@gmail.com>
2022-06-29 23:37:12 +0530

    fix: added env file in database.js

--- database.js
+++ database.js
@@ -1,8 +1,10 @@
 const Pool = require('pg').Pool;
+require('dotenv').config();
+
 
 const pool = new Pool({
     user: "postgres",
-    password: "vaidikvaidik",
+    password: process.env.postgresPassword,
     host: "localhost",
     port: 5432,
     database: "musicapp"
