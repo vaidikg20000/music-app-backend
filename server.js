60d1853bdf27a7963cd457d1cd998317c3a368bf
Vaidik Gupta <vaidikgupta2000@gmail.com>
2022-07-01 23:43:20 +0530

    feat: added port console log

--- server.js
+++ server.js
@@ -17,5 +17,5 @@ app.get("/",(req,res)=>{
 
 
 app.listen(process.env.PORT || 3000 , function() {
-    console.log('listening on 3000');
+    console.log('listening on ' + process.env.PORT || 3000);
 })
